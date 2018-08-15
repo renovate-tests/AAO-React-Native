123a61ab6c98e489a999312aac57f4100c74f2cb
Hawken Rives <hawkrives@fastmail.fm>
2018-08-14 21:22:38 -0500

    start doing rnn in JS

--- source/root.js
+++ source/root.js
@@ -1,7 +1,9 @@
 // @flow
 
-import {AppRegistry, YellowBox} from 'react-native'
-import App from './app'
+import * as React from 'react'
+import {YellowBox, View, Text} from 'react-native'
+import {Navigation} from 'react-native-navigation'
+// import App from './app'
 
 // I'm not importing the exported variable because I just want to initialize
 // the file here.
@@ -14,4 +16,55 @@ YellowBox.ignoreWarnings([
 	'Module RCTImageLoader',
 ])
 
-AppRegistry.registerComponent('AllAboutOlaf', () => App)
+class LifecycleScreenExample extends React.Component<any, any> {
+	constructor(props) {
+		super(props)
+		Navigation.events().bindComponent(this)
+	}
+
+	state = {
+		text: 'nothing yet',
+	}
+
+	componentWillUnmount() {
+		alert('componentWillUnmount')
+	}
+
+	componentDidAppear() {
+		this.setState(() => ({text: 'componentDidAppear'}))
+	}
+
+	componentDidDisappear() {
+		alert('componentDidDisappear')
+	}
+
+	navigationButtonPressed({buttonId}) {
+		// a navigation-based button (for example in the topBar) was clicked. See section on buttons.
+		alert(buttonId)
+	}
+
+	render() {
+		return (
+			<View>
+				<Text>Lifecycle Screen</Text>
+				<Text>{this.state.text}</Text>
+			</View>
+		)
+	}
+}
+
+// AppRegistry.registerComponent('AllAboutOlaf', () => App)
+Navigation.registerComponent(
+	'navigation.playground.WelcomeScreen',
+	() => LifecycleScreenExample,
+)
+
+Navigation.events().registerAppLaunchedListener(() => {
+	Navigation.setRoot({
+		root: {
+			component: {
+				name: 'navigation.playground.WelcomeScreen',
+			},
+		},
+	})
+})
